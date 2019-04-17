c36e3822c599c3c7aa2ca5eea371a9d7cb88ee89
Lin, Ying-Chin <mirror770109@gmail.com>
2019-04-17 11:37:03 +0800

    Add event colors

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"golang.org/x/net/context"
@@ -17,9 +18,11 @@ import (
 
 // Calendar ..
 type Calendar struct {
-	ID         string
-	Summary    string
-	Department string
+	ID           string
+	Summary      string
+	Department   string
+	ColorID      int
+	SubCalendars []Calendar
 }
 
 // CalendarContext ...
@@ -30,22 +33,26 @@ type CalendarContext struct {
 
 func (calCtx *CalendarContext) loadCalendarList() {
 	calCtx.Calendars = []Calendar{
-		Calendar{Department: "文檔", Summary: "0. 文檔科行事曆", ID: "6a9uk9gc2d15udchreqeq3k1rk@group.calendar.google.com"},
-		Calendar{Department: "總務", Summary: "1. 總務科行事曆", ID: "ktmlkpioqff9nu111p7as526v4@group.calendar.google.com"},
-		Calendar{Department: "公關", Summary: "2. 公共關係科行事曆", ID: "o4bkrddgv0b4bq6b5j6kpedae8@group.calendar.google.com"},
-		Calendar{Department: "國際", Summary: "3. 國際事務科行事曆", ID: "iidlj6hgoelq6s9rsumob02rrc@group.calendar.google.com"},
-		Calendar{Department: "機要", Summary: "4. 機要科行事曆", ID: "h55qgjl1llcbuu0aqqf71hur2o@group.calendar.google.com"},
-		Calendar{Department: "廳舍", Summary: "5. 廳舍管理科行事曆", ID: "ts9chk929t0jponh9cegafp4m8@group.calendar.google.com"},
-		Calendar{Department: "採購", Summary: "6. 採購管理科行事曆", ID: "qcugjootlce6q62qubtsi4okqg@group.calendar.google.com"},
-		Calendar{Department: "人事", Summary: "7. 人事室行事曆", ID: "r7uqipsmab6dseejdcur39f2nc@group.calendar.google.com"},
-		Calendar{Department: "會計", Summary: "8. 會計室行事曆", ID: "uhaeie6j2sinimlr8r6196v0rc@group.calendar.google.com"},
-		Calendar{Department: "政風", Summary: "9. 政風室行事曆", ID: "in5cciirl6oakjiccac1l1k0l0@group.calendar.google.com"},
+		Calendar{Department: "綜合", Summary: "秘書處行事曆", ID: "primary", ColorID: 1},
+		Calendar{Department: "一層", Summary: "0. 處本部主管行事曆", ID: "l3gc5npg7m05a730gqgeobrcfo@group.calendar.google.com", ColorID: 1},
+		Calendar{Department: "二層", Summary: "1. 處科室主管行事曆", ID: "3a9gmj05p8f8cnutlus1qc7ub4@group.calendar.google.com", ColorID: 1, SubCalendars: []Calendar{
+			Calendar{Department: "文檔", Summary: "10. 文檔科行事曆", ID: "6a9uk9gc2d15udchreqeq3k1rk@group.calendar.google.com", ColorID: 2},
+			Calendar{Department: "總務", Summary: "11. 總務科行事曆", ID: "ktmlkpioqff9nu111p7as526v4@group.calendar.google.com", ColorID: 3},
+			Calendar{Department: "公關", Summary: "12. 公共關係科行事曆", ID: "o4bkrddgv0b4bq6b5j6kpedae8@group.calendar.google.com", ColorID: 4},
+			Calendar{Department: "國際", Summary: "13. 國際事務科行事曆", ID: "iidlj6hgoelq6s9rsumob02rrc@group.calendar.google.com", ColorID: 5},
+			Calendar{Department: "機要", Summary: "14. 機要科行事曆", ID: "h55qgjl1llcbuu0aqqf71hur2o@group.calendar.google.com", ColorID: 6},
+			Calendar{Department: "廳舍", Summary: "15. 廳舍管理科行事曆", ID: "ts9chk929t0jponh9cegafp4m8@group.calendar.google.com", ColorID: 7},
+			Calendar{Department: "採購", Summary: "16. 採購管理科行事曆", ID: "qcugjootlce6q62qubtsi4okqg@group.calendar.google.com", ColorID: 8},
+			Calendar{Department: "人事", Summary: "17. 人事室行事曆", ID: "r7uqipsmab6dseejdcur39f2nc@group.calendar.google.com", ColorID: 9},
+			Calendar{Department: "會計", Summary: "18. 會計室行事曆", ID: "uhaeie6j2sinimlr8r6196v0rc@group.calendar.google.com", ColorID: 10},
+			Calendar{Department: "政風", Summary: "19. 政風室行事曆", ID: "in5cciirl6oakjiccac1l1k0l0@group.calendar.google.com", ColorID: 11},
+		}},
 	}
 }
 
 func (calCtx *CalendarContext) getLastUpdatedCalendarEvents(cal Calendar, in chan<- []*calendar.Event) {
 	for {
-		t := time.Now().Add(-24 * time.Second).Format(time.RFC3339)
+		t := time.Now().Add(-12 * time.Second).Format(time.RFC3339)
 		events, err := calCtx.Service.Events.List(cal.ID).UpdatedMin(t).Do()
 		if err != nil {
 			log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
@@ -58,13 +65,14 @@ func (calCtx *CalendarContext) getLastUpdatedCalendarEvents(cal Calendar, in cha
 			for _, item := range events.Items {
 				fmt.Printf("%v (%v, %v)\n", item.Summary, item.Id, item.Updated)
 				item.Summary = fmt.Sprintf("[%s] %s", cal.Department, item.Summary)
+				item.ColorId = strconv.Itoa(cal.ColorID)
 			}
 
 			in <- events.Items
 		}
 		fmt.Println()
 
-		time.Sleep(10 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
 
@@ -169,12 +177,13 @@ func main() {
 	calCtx := CalendarContext{Service: srv}
 	calCtx.loadCalendarList()
 
-	events := make(chan []*calendar.Event)
-
-	go calCtx.syncLastUpdatedCalendarEvents("l3gc5npg7m05a730gqgeobrcfo@group.calendar.google.com", events)
-
 	for _, cal := range calCtx.Calendars {
-		go calCtx.getLastUpdatedCalendarEvents(cal, events)
+		events := make(chan []*calendar.Event)
+		go calCtx.syncLastUpdatedCalendarEvents(cal.ID, events)
+
+		for _, subCal := range cal.SubCalendars {
+			go calCtx.getLastUpdatedCalendarEvents(subCal, events)
+		}
 	}
 
 	abort := make(chan bool)
