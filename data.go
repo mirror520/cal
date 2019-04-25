153c583a59b41e0d2b9db173a18b19a28df6a9ef
Lin, Ying-Chin <mirror770109@gmail.com>
2019-04-25 17:38:13 +0800

    Move calendars to data

---
+++ data.go
@@ -0,0 +1,18 @@
+package main
+
+var calData = []Calendar{
+	Calendar{Prefix: "綜合", Summary: "秘書處行事曆", ID: "primary", ColorID: 1},
+	Calendar{Prefix: "一層", Summary: "0. 處本部主管行事曆", ID: "l3gc5npg7m05a730gqgeobrcfo@group.calendar.google.com", ColorID: 1, Members: []Member{}},
+	Calendar{Prefix: "二層", Summary: "1. 處科室主管行事曆", ID: "3a9gmj05p8f8cnutlus1qc7ub4@group.calendar.google.com", ColorID: 1, SubCalendars: []Calendar{
+		Calendar{Prefix: "文檔", Summary: "10. 文檔科行事曆", ID: "6a9uk9gc2d15udchreqeq3k1rk@group.calendar.google.com", ColorID: 2, Members: []Member{}},
+		Calendar{Prefix: "總務", Summary: "11. 總務科行事曆", ID: "ktmlkpioqff9nu111p7as526v4@group.calendar.google.com", ColorID: 3, Members: []Member{}},
+		Calendar{Prefix: "公關", Summary: "12. 公共關係科行事曆", ID: "o4bkrddgv0b4bq6b5j6kpedae8@group.calendar.google.com", ColorID: 4, Members: []Member{}},
+		Calendar{Prefix: "國際", Summary: "13. 國際事務科行事曆", ID: "iidlj6hgoelq6s9rsumob02rrc@group.calendar.google.com", ColorID: 5, Members: []Member{}},
+		Calendar{Prefix: "機要", Summary: "14. 機要科行事曆", ID: "h55qgjl1llcbuu0aqqf71hur2o@group.calendar.google.com", ColorID: 6, Members: []Member{}},
+		Calendar{Prefix: "廳舍", Summary: "15. 廳舍管理科行事曆", ID: "ts9chk929t0jponh9cegafp4m8@group.calendar.google.com", ColorID: 7, Members: []Member{}},
+		Calendar{Prefix: "採購", Summary: "16. 採購管理科行事曆", ID: "qcugjootlce6q62qubtsi4okqg@group.calendar.google.com", ColorID: 8, Members: []Member{}},
+		Calendar{Prefix: "人事", Summary: "17. 人事室行事曆", ID: "r7uqipsmab6dseejdcur39f2nc@group.calendar.google.com", ColorID: 9, Members: []Member{}},
+		Calendar{Prefix: "會計", Summary: "18. 會計室行事曆", ID: "uhaeie6j2sinimlr8r6196v0rc@group.calendar.google.com", ColorID: 10, Members: []Member{}},
+		Calendar{Prefix: "政風", Summary: "19. 政風室行事曆", ID: "in5cciirl6oakjiccac1l1k0l0@group.calendar.google.com", ColorID: 11, Members: []Member{}},
+	}},
+}
